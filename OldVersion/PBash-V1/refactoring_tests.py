bb14256eea95631e92d11590c93c134c2e977799
Jiyun Wan <rosemary1171010@gmail.com>
2017-11-11 08:05:06 +1300

    add the unit tests for refactoring file_handler and import file_reader
    refactoring_test.py

--- OldVersion/PBash-V1/refactoring_tests.py
+++ OldVersion/PBash-V1/refactoring_tests.py
@@ -1,9 +1,49 @@
 import unittest
 from validator import Validator
+from file_reader import *
+from file_handler import *
+
+class FileReaderTests(unittest.TestCase):
+    def test_csv(self):
+        fh = FileHandler(Validator())
+        actual = fh.open('testfile/csvTest.csv')[0]
+        expected = {'EMPID': 'A001',
+                    'GENDER': 'F',
+                    'AGE': '21',
+                    'SALES': '001',
+                    'BMI': 'Normal',
+                    'SALARY': '12',
+                    'BIRTHDAY': '1-1-1996'}
+        self.assertEquals(actual, expected)
+
+    def test_txt(self):
+        fh = FileHandler(Validator())
+        actual = fh.open('testfile/txtTest.txt')[0]
+        expected = {'EMPID': 'A001',
+                    'GENDER': 'F',
+                    'AGE': '21',
+                    'SALES': '001',
+                    'BMI': 'Normal',
+                    'SALARY': '12',
+                    'BIRTHDAY': '1-1-1996'}
+        self.assertEquals(actual, expected)
+
+    def test_xlsx(self):
+        fh = FileHandler(Validator())
+        actual = fh.open('testfile/testingdata.xlsx')[0]
+
+        expected = {'EMPID': 'A001',
+                    'GENDER': 'F',
+                    'AGE': '21',
+                    'SALES': '001',
+                    'BMI': 'Normal',
+                    'SALARY': '12',
+                    'BIRTHDAY': '1-1-1996'}
+        self.assertEquals(actual, expected)
 
 
 
 
 
-suite = unittest.TestLoader().loadTestsFromTestCase(RefactorTests)
+suite = unittest.TestLoader().loadTestsFromTestCase(FileReaderTestsTests)
 unittest.TextTestRunner(verbosity=1).run(suite)
